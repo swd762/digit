6ccf588f4ff21bf184450e1809298dc11dab4bb6
Kazalin <shadow762@list.ru>
2020-12-24 12:19:59 +0300

    changed texts

--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -1,35 +1,39 @@
 <!--Шаблон панели администратора-->
 <template>
-    <div class="card">
-        <h2>Панель Администратора</h2>
-        <div class="table">
-            <usersList></usersList>
-        </div>
+  <div class="card">
+    <h2>Панель Администратора</h2>
+    <p>
+      Здесь будут различные настройки системы, управление пользователями и
+      прочее
+    </p>
+    <div class="table">
+      <usersList></usersList>
     </div>
+  </div>
 </template>
 
 <script>
 // подгрузка компонента списка пользователей
-import usersList from '../../components/Users-list.vue'
+import usersList from "../../components/Users-list.vue";
 export default {
-    name: "Dashboard",
-    // регистрация компонента списка пользователей
-    components: {
-        usersList
-    }
-}
+  name: "Dashboard",
+  // регистрация компонента списка пользователей
+  components: {
+    usersList,
+  },
+};
 </script>
 <style>
 .card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .card h2 {
-    margin: 10px;
+  margin: 10px;
 }
 
 .table {
-    display: flex;
+  display: flex;
 }
 </style>
