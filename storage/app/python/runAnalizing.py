70ec144b1c0a6c75863ec2c5a322dda32113b148
Kazalin <shadow762@list.ru>
2021-02-21 19:22:32 +0300

    Model saving

--- storage/app/python/runAnalizing.py
+++ storage/app/python/runAnalizing.py
@@ -2,7 +2,12 @@ import pymysql
 import sys
 import json
 
-# dateRange = sys.argv[1]
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+
+dateRange = sys.argv[1]
 dates = json.loads(sys.argv[1])
 
 print(dates)
@@ -48,6 +53,9 @@ with connection:
 
         print(result)
 
+        # Получаем данные за указанный период. В цикле оцениваем каждый набор данных. Записываем результат оценки обратно в таблицу
+
+
 # import psycopg2
 # from sqlalchemy import create_engine
 
@@ -65,3 +73,63 @@ with connection:
 # for row in cur.fetchall():
 #     #print(row)
 #     #...
+
+
+checkpoint_path = "training_1/cp.ckpt"
+df = pd.read_csv('./data.csv')
+
+features = df.drop('true', axis=1)
+labels = df['true']
+
+feature_train, feature_test, label_train, label_test = train_test_split(
+    features, labels, test_size=0.33)
+
+# Зарезервируем 100 примеров для валидации
+feature_val = feature_train[-100:]  # последние 100
+label_val = label_train[-100:]
+
+feature_train = feature_train[:-100]  # все, кроме последних 100
+label_train = label_train[:-100]
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(
+    # optimizer=keras.optimizers.RMSprop(),  # Optimizer
+    optimizer='adam',  # Optimizer
+              # Минимизируемая функция потерь
+              loss=keras.losses.SparseCategoricalCrossentropy(),
+              # Список метрик для мониторинга
+              metrics=[keras.metrics.SparseCategoricalAccuracy()])
+
+# # Оценим необученную модель на тестовых данных, используя "evaluate"
+# print('\n# Оцениваем на тестовых данных')
+# results = model.evaluate(feature_test, label_test, batch_size=32)
+# print('test loss, test acc:', results)
+
+model.load_weights(checkpoint_path)
+
+# Оценим модель на тестовых данных, используя "evaluate"
+print('\n# Оцениваем на тестовых данных')
+results = model.evaluate(feature_test, label_test, batch_size=32)
+print('test loss, test acc:', results)
+
+
+# Сгенерируем прогнозы (вероятности - выходные данные последнего слоя)
+# на новых данных с помощью "predict"
+print('\n# Генерируем прогнозы для 3 образцов')
+predictions = model.predict(feature_test[:3])
+print('размерность прогнозов:', predictions.shape)
+print('прогнозы:', predictions)
+
+
+probability_model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.Softmax()
+])
+
+
+probability_model(feature_test[:5])
--- storage/app/python/trainModel.py
+++ storage/app/python/trainModel.py
@@ -1,9 +1,19 @@
+import os
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('./data.csv')
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
 
 features = df.drop('true', axis=1)
 labels = df['true']
@@ -22,17 +32,23 @@ label_train = label_train[:-100]
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(1)
 ])
 
-model.compile(optimizer=keras.optimizers.RMSprop(),  # Optimizer
-              # Минимизируемая функция потерь
-              loss=keras.losses.SparseCategoricalCrossentropy(),
-              # Список метрик для мониторинга
-              metrics=[keras.metrics.SparseCategoricalAccuracy()])
+model.compile(
+    optimizer=keras.optimizers.RMSprop(),  # Optimizer
+    # optimizer='adam',  # Optimizer
+    # Минимизируемая функция потерь
+    # loss=keras.losses.SparseCategoricalCrossentropy(),
+    loss=keras.losses.BinaryCrossentropy(),
+    # Список метрик для мониторинга
+    # metrics=[keras.metrics.SparseCategoricalAccuracy()])
+    metrics=[keras.metrics.BinaryAccuracy()])
 
 history = model.fit(feature_train, label_train, batch_size=32,
-                    epochs=10, validation_data=(feature_val, label_val))
+                    epochs=10, validation_data=(feature_val, label_val), callbacks=[cp_callback])
+
+model.summary()
 
 print('\nhistory dict:', history.history)
 
@@ -44,5 +60,16 @@ print('test loss, test acc:', results)
 # Сгенерируем прогнозы (вероятности - выходные данные последнего слоя)
 # на новых данных с помощью "predict"
 print('\n# Генерируем прогнозы для 3 образцов')
+print('Образцы:', feature_test[:3])
 predictions = model.predict(feature_test[:3])
 print('размерность прогнозов:', predictions.shape)
+print('Прогнозы:', predictions)
+
+
+probability_model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.Softmax()
+])
+
+
+probability_model(feature_test[:5])
