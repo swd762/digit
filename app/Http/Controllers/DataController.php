ea31a0168e9965fafa6dee82d901954e52010f52
kazalin <shadow762@list.ru>
2021-02-25 13:27:51 +0300

    added logs

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Module;
 use App\Models\ModuleData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DataController extends Controller
 {
@@ -12,6 +13,17 @@ class DataController extends Controller
 
     public function import(Request $request)
     {
+        // ModuleData::create([
+        //     'user_id' => 1,
+        //     'module_id' => 1,
+        //     'temperature' => 1,
+        //     'bend' => 1
+        // ]);
+
+        Log::info('module id: ' . $request->id);
+        Log::info('data: ' . $request->data);
+        Log::info('all: ' . json_encode($request->all()));
+
         return response()->json([
             'status' => 'success',
         ], 200);
