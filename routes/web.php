a0712ec890d0ded835bcf56313783bfb6731a73c
kazalin <shadow762@list.ru>
2021-03-09 14:15:26 +0300

    fixed base route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route * to vue router template
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Route::get('{path?}', function () {
     return view('index');
